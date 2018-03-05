05643f7e236acb58215b96835e5a9b2d6eb943a1
Nicky Fusté <nicky.fuste@gmail.com>
2018-03-05 14:35:51 +0100

    First approach

---
+++ ape.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := "http://fotocasa.es"
+	fmt.Printf("HTML code of %s ...\n", url)
+	resp, err := http.Get(url)
+	// handle the error if there is one
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// do this now so it won't be forgotten
+	defer resp.Body.Close()
+	// reads html as a slice of bytes
+	html, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// show the HTML code as a string %s
+	fmt.Printf("%s\n", html)
+}
