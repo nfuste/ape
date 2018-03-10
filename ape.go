86b5d7cb678a88523629db75a623c196fe25f917
Nicky Fusté <nicky.fuste@gmail.com>
2018-03-10 18:58:36 +0100

    func saveToken added

--- ape.go
+++ ape.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/oauth2"
 )
+
+// saveToken uses a file path to create a file and store the
+// token in it.
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
 
 func printInfo(i int, s *goquery.Selection) {
 	href, _ := s.Attr("href")
